d3f233f2c88716d782b699fa7d5b357041f4190e
Nicholas Sylke <nicksylke@outlook.com>
2018-10-06 17:48:47 -0500

    Set up basic container and set log level to `INFO`

--- src/main/java/com/typicalbot/TypicalBot.java
+++ src/main/java/com/typicalbot/TypicalBot.java
@@ -17,16 +17,27 @@
  */
 package com.typicalbot;
 
+import com.typicalbot.common.Container;
+import com.typicalbot.common.Logger;
+import com.typicalbot.common.facade.ContainerFacade;
 import com.typicalbot.shard.ShardManager;
+import org.apache.log4j.Level;
 
 import javax.security.auth.login.LoginException;
 
 public class TypicalBot {
-    public static void main(String[] args) {
-        try {
-            ShardManager.register(1);
-        } catch (LoginException | InterruptedException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws LoginException, InterruptedException {
+        // We will have to move this container facade outside of the main method
+        // because we will need two different facades. The first one, this, will
+        // be the global facade to handle various services that will be used in
+        // different sections of the bot, and then the second one, shard, will be
+        // used to handle minor services that are not used frequently. This should
+        // essentially stop services from stalling when a shard stops unexpectedly.
+        ContainerFacade container = new ContainerFacade();
+        Container.swap(container);
+
+        Logger.init(Level.INFO);
+
+        ShardManager.register(1);
     }
 }
