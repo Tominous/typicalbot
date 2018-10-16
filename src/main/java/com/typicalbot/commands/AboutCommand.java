72fe2e1324ad40975d7d71d84c497400c291a458
Nicholas Sylke <nicksylke@outlook.com>
2018-10-16 08:58:20 -0500

    Add command arguments

--- src/main/java/com/typicalbot/commands/AboutCommand.java
+++ src/main/java/com/typicalbot/commands/AboutCommand.java
@@ -2,12 +2,13 @@ package com.typicalbot.commands;
 
 import com.typicalbot.command.Command;
 import com.typicalbot.command.CommandListener;
+import com.typicalbot.command.CommandSource;
 
 /**
  * @since 3.0.0
  */
 public class AboutCommand implements CommandListener {
     @Command(name = "about", usage = "$about")
-    public void about() {
+    public void about(CommandSource source, String[] args) {
     }
 }
--- src/main/java/com/typicalbot/commands/CreditsCommand.java
+++ src/main/java/com/typicalbot/commands/CreditsCommand.java
@@ -2,12 +2,13 @@ package com.typicalbot.commands;
 
 import com.typicalbot.command.Command;
 import com.typicalbot.command.CommandListener;
+import com.typicalbot.command.CommandSource;
 
 /**
  * @since 3.0.0
  */
 public class CreditsCommand implements CommandListener {
     @Command(name = "credits", aliases = {"credit"}, usage = "$credits")
-    public void credit() {
+    public void credit(CommandSource source, String[] args) {
     }
 }
