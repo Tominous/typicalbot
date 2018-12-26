2d860a3da1c9a2e8337104cfa232fdf8b8cf3bb4
Nicholas Sylke <hello@nsylke.me>
2018-12-25 20:11:03 -0600

    Remove excess whitespace in DataSerializer

--- src/main/java/com/typicalbot/data/DataSerializer.java
+++ src/main/java/com/typicalbot/data/DataSerializer.java
@@ -19,7 +19,7 @@ import java.io.*;
 
 public class DataSerializer {
     // TODO: Separate the two methods, and make interfaces.
-    
+
     public void serialize(Object object, OutputStream stream) throws IOException {
         if (!(object instanceof Serializable)) {
             throw new IllegalArgumentException();
