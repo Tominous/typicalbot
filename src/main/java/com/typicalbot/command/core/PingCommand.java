0a508bfb40076a8acd584f865ecd4f0dfc24a2ec
Nicholas Sylke <hello@nsylke.me>
2018-12-31 16:35:16 -0600

    Add PingCommand as sample

---
+++ src/main/java/com/typicalbot/command/core/PingCommand.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2016-2018 Bryan Pikaard & Nicholas Sylke
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.typicalbot.command.core;
+
+import com.typicalbot.command.BaseCommand;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.User;
+
+public class PingCommand extends BaseCommand {
+    public PingCommand() {
+        super("ping");
+    }
+
+    @Override
+    public void onExecute(Guild guild, TextChannel channel, Message message, User author, String[] args) {
+        message.getChannel().sendMessage("Pong!").queue();
+    }
+}
