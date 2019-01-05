661729239f078bad726f41e46929d55f4fafb3d9
AKSKL <AKSKL@users.noreply.github.com>
2019-01-05 11:39:48 -0500

    Updated formatting of user/server commands and added simple documentation

--- src/main/java/com/typicalbot/command/utility/ServerCommand.java
+++ src/main/java/com/typicalbot/command/utility/ServerCommand.java
@@ -30,39 +30,37 @@ import java.util.stream.Collectors;
 public class ServerCommand implements Command {
     @Override
     public void execute(CommandContext context, CommandArgument argument) {
+        /* VARIABLES TO RETURN IN THE FINAL MESSAGE */
         String header = "**__Server information for:__** " + context.getMessage().getGuild().getName() + "\n";
         String name = "Name                : " + context.getMessage().getGuild().getName() + " (" + context.getMessage().getGuild().getId() + ")\n";
         String owner = "Owner               : " + context.getMessage().getGuild().getOwner().getUser().getName() + "#" + context.getMessage().getGuild().getOwner().getUser().getDiscriminator() + "\n";
         //TODO(AKSKL): Format creation date into a more 'readable' string
         String created = "Created             : " + context.getMessage().getGuild().getCreationTime().toString() + "\n";
         String region = "Region              : " + context.getMessage().getGuild().getRegion().toString() + "\n";
-        //String verification_level
+        //TODO: Verification level?
         String icon = "Icon                : " + context.getMessage().getGuild().getIconUrl() + "\n";
         String numOfChannels = "Channels            : " + context.getMessage().getGuild().getChannels().size() + "\n";
         String numOfMembers = "Members             : " + context.getMessage().getGuild().getMembers().size() + "\n";
         String numOfRoles = "Roles               : " + context.getMessage().getGuild().getRoles().size() + "\n";
         String numOfEmotes = "Emotes              : " + context.getMessage().getGuild().getEmotes().size() + "\n"; //changed "emoji" to "emotes" in the rewrite
+
+        /* FINAL MESSAGE (put into a code block */
         context.sendMessage(header + "```" + name + owner + created + region + icon + numOfChannels + numOfMembers + numOfRoles + numOfEmotes + "```");
     }
 
     @Override
     public void embed(CommandContext context, CommandArgument argument) {
+        /* VARIABLES TO RETURN IN THE FINAL MESSAGE */
         String name = context.getMessage().getGuild().getName() + " (" + context.getMessage().getGuild().getId() + ")";
         String owner = context.getMessage().getGuild().getOwner().getUser().getName() + "#" + context.getMessage().getGuild().getOwner().getUser().getDiscriminator() + " (" + context.getMessage().getGuild().getOwner().getUser().getId() + ")";
         String created = context.getMessage().getGuild().getCreationTime().toString();
         String region = context.getMessage().getGuild().getRegion().toString();
-        String numOfChannels = Integer.toString(context.getMessage().getGuild().getChannels().size());
-        String numOfMembers = Integer.toString(context.getMessage().getGuild().getMembers().size());
+        int numOfChannels = context.getMessage().getGuild().getChannels().size();
+        int numOfMembers = context.getMessage().getGuild().getMembers().size();
+        int numOfRoles = context.getMessage().getGuild().getRoles().size();
+        int numOfEmotes = context.getMessage().getGuild().getEmotes().size(); //changed "emoji" to "emotes" in the rewrite
 
-        String numOfRoles = Integer.toString(context.getMessage().getGuild().getRoles().size());
-        List<Role> rolesList = context.getMessage().getGuild().getRoles();
-        String roles = rolesList.stream().map(Role::getName).collect(Collectors.joining(", "));
-        roles = removeEveryoneMentions(roles);
-
-        String numOfEmotes = Integer.toString(context.getMessage().getGuild().getEmotes().size()); //changed "emoji" to "emotes" in the rewrite
-        List<Emote> emotesList = context.getMessage().getGuild().getEmotes();
-        String emotes = emotesList.stream().map(Emote::getName).collect(Collectors.joining(", "));
-
+        /* Begin building the embed */
         EmbedBuilder embed = new EmbedBuilder()
                 .setColor(0x00ADFF)
                 .setTitle("Server Information")
@@ -70,29 +68,51 @@ public class ServerCommand implements Command {
                 .addField("» Owner", owner, false)
                 .addField("» Created", created, true)
                 .addField("» Region", region, true)
-                .addField("» Channels", numOfChannels, true)
-                .addField("» Members", numOfMembers, true);
+                .addField("» Channels", Integer.toString(numOfChannels), true)
+                .addField("» Members", Integer.toString(numOfMembers), true);
 
-        if (Integer.parseInt(numOfRoles) > 0) {
+        /* Both roles and emotes can be listed, but we must first check to see if there are any to begin with. */
+        //Roles
+        if (numOfRoles > 0) {
+            //This converts the Role objects into a displayable list
+            List<Role> rolesList = context.getMessage().getGuild().getRoles();
+            String roles = rolesList.stream().map(Role::getName).collect(Collectors.joining(", "));
+
+            //Removes the everyone ping
+            roles = removeEveryoneMentions(roles);
+
+            //Adds the roles list to the embed
             embed.addField("» Roles", roles, false);
         } else {
-            embed.addField("» Roles", numOfRoles, true);
+            //Since here there are no roles, we will just return the number of roles, which is zero (This theoretically should never happen)
+            embed.addField("» Roles", Integer.toString(numOfRoles), true);
         }
 
-        if (Integer.parseInt(numOfEmotes) > 0) {
+        //Emotes
+        if (numOfEmotes > 0) {
+            //This converts the Emote objects into a displayable list
+            List<Emote> emotesList = context.getMessage().getGuild().getEmotes();
+            String emotes = emotesList.stream().map(Emote::getName).collect(Collectors.joining(", "));
+
+            //Adds the emotes list to the embed
             embed.addField("» Emotes", emotes, false);
         } else {
-            embed.addField("» Emotes", numOfEmotes, true);
+            //Since there are no emotes, we will return the number of emotes, which is zero
+            embed.addField("» Emotes", Integer.toString(numOfEmotes), true);
         }
 
+        //Finish off the embed with the thumbnail and footer
         embed
                 .setThumbnail(context.getMessage().getGuild().getIconUrl())
                 //TODO(nsylke): make the TB icon a constant
                 .setFooter("TypicalBot", "https://images-ext-2.discordapp.net/external/qYPuNcjM4PjaKvsmlc-lcHhtJ8RZ-txaxYMDQmWL0g8/https/www.typicalbot.com/x/images/icon.png");
+
+        //Send the embed
         context.sendEmbed(embed.build());
     }
 
     public String removeEveryoneMentions(String input) {
-        return input.replace("@everyone", "everyone");
+        //\u200B is a zero-width space, which nullifies the mention
+        return input.replace("@everyone", "@\u200Beveryone");
     }
 }
--- src/main/java/com/typicalbot/command/utility/UserCommand.java
+++ src/main/java/com/typicalbot/command/utility/UserCommand.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -32,9 +32,11 @@ import java.util.stream.Collectors;
 public class UserCommand implements Command {
     @Override
     public void execute(CommandContext context, CommandArgument argument) {
+        /* Set up easy Member and User objects for the mentioned person */
         Member mentionedMember = getMember(context.getMessage());
         User mentionedUser = mentionedMember.getUser();
 
+        /* VARIABLES TO RETURN IN THE FINAL MESSAGE */
         String header = "**__User information for:__** " + mentionedUser.getName() + "\n";
         String name = "Name                : " + mentionedUser.getName() + "#" + mentionedUser.getDiscriminator() + " (" + mentionedUser.getId() + ")\n";
         //TODO(AKSKL): Make status prettier
@@ -50,28 +52,29 @@ public class UserCommand implements Command {
 
     @Override
     public void embed(CommandContext context, CommandArgument argument) {
-
-        //TODO(nsylke): Format embed such that "Joined Discord" and "Joined Server" are always adjacent.
-
         Member mentionedMember = getMember(context.getMessage());
         User mentionedUser = mentionedMember.getUser();
 
+        /* VARIABLES TO RETURN IN THE FINAL MESSAGE */
         String header = "User information for: " + mentionedUser.getName();
         String tag = mentionedUser.getName() + "#" + mentionedUser.getDiscriminator();
         String id = mentionedUser.getId();
         String status = mentionedMember.getOnlineStatus().toString();
         String joinDate = mentionedUser.getCreationTime().toString();
         String serverJoinDate = mentionedMember.getJoinDate().toString();
+        int numOfRoles = mentionedMember.getRoles().size();
 
-        List<Role> rolesList = mentionedMember.getRoles();
-        String roles = rolesList.stream().map(Role::getName).collect(Collectors.joining(", "));
-
+        /* Begin building the embed */
         EmbedBuilder embed = new EmbedBuilder()
                 .setTitle(header)
                 .addField("» Tag", tag, true)
                 .addField("» ID", id, true)
+                .addField("» Joined Discord", joinDate, true)
+                .addField("» Joined Server", serverJoinDate, true)
                 //TODO(AKSKL): Make status prettier
                 .addField("» Status", status, true);
+
+        // Both a member's presence and their nickname may not exist, so we have to check first
         if (mentionedMember.getGame() != null) {
             String presence = mentionedMember.getGame().getName();
             embed.addField("» Playing", presence, true);
@@ -80,21 +83,30 @@ public class UserCommand implements Command {
             String nickname = mentionedMember.getNickname();
             embed.addField("» Nickname", nickname, true);
         }
-        embed
-                .addField("» Joined Discord", joinDate, true)
-                .addField("» Joined Server", serverJoinDate, true)
-                .addField("» Roles", roles, false)
-                .setThumbnail(mentionedUser.getAvatarUrl())
+
+        //Since a member may not have any roles, we must check that they have some
+        if (numOfRoles > 0) {
+            List<Role> rolesList = mentionedMember.getRoles();
+            String roles = rolesList.stream().map(Role::getName).collect(Collectors.joining(", "));
+            embed.addField("» Roles", roles, true);
+        }
+
+        //Finish off the embed with the thumbnail and footer
+        embed.setThumbnail(mentionedUser.getAvatarUrl())
                 //TODO(nsylke): make the TB icon a constant
                 .setFooter("TypicalBot", "https://images-ext-2.discordapp.net/external/qYPuNcjM4PjaKvsmlc-lcHhtJ8RZ-txaxYMDQmWL0g8/https/www.typicalbot.com/x/images/icon.png");
 
+        //Send the embed
         context.sendEmbed(embed.build());
     }
 
+    //A simple function to determine the target of the command
     public Member getMember(Message message) {
         if (message.getMentionedMembers().isEmpty()) {
+            //if the user didn't mention anyone, the target is themself
             return message.getMember();
         } else {
+            //if the user mentioned other users, the target is assumed to be the first member mentioned
             return message.getMentionedMembers().get(0);
         }
     }
