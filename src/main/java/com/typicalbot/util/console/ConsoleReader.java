74f9d6d801e23b2b5026e84806a0e091c1adf5ab
Nicholas Sylke <hello@nsylke.me>
2019-01-14 08:29:17 -0600

    Move ConsoleReader to util package

--- src/main/java/com/typicalbot/util/console/ConsoleReader.java
+++ src/main/java/com/typicalbot/util/console/ConsoleReader.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.typicalbot.console;
+package com.typicalbot.util.console;
 
 import java.io.DataInputStream;
 import java.io.IOException;
