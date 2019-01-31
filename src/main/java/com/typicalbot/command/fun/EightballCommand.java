854d11d7bcca1595d6cdd4211557c3069a253ed2
Nicholas Sylke <hello@nsylke.me>
2019-01-31 11:22:33 -0600

    Move responses out of method

--- src/main/java/com/typicalbot/command/fun/EightballCommand.java
+++ src/main/java/com/typicalbot/command/fun/EightballCommand.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,10 +26,33 @@ import java.util.Random;
 
 @CommandConfiguration(category = CommandCategory.FUN, aliases = {"8ball", "eightball"})
 public class EightballCommand implements Command {
+    private String[] responses = new String[]{
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",
+        "Yes, definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook is good.",
+        "Yes.",
+        "Signs point to yes.",
+        "Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Very doubtful."
+    };
+
     @Override
     public String[] usage() {
         return new String[]{
-                "8ball <question>"
+            "8ball <question>"
         };
     }
 
@@ -50,7 +73,6 @@ public class EightballCommand implements Command {
             return;
         }
 
-        String[] responses = new String[]{"It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook is good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."};
         context.sendMessage(responses[new Random().nextInt(responses.length)]);
     }
 }
