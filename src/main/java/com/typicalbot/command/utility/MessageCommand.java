e1f8738d4613bea925ef8ba5014ccabfa8b52b2c
Nicholas Sylke <hello@nsylke.me>
2019-02-03 09:12:30 -0600

    Remove useless return

--- src/main/java/com/typicalbot/command/utility/MessageCommand.java
+++ src/main/java/com/typicalbot/command/utility/MessageCommand.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -49,11 +49,10 @@ public class MessageCommand implements Command {
 
             context.sendEmbed(builder.build());
         } catch (ErrorResponseException errorResponse) {
-            if(errorResponse.getErrorCode() == 10008) {
+            if (errorResponse.getErrorCode() == 10008) {
                 context.sendMessage("Error: Either an invalid message or the message isn't from this channel (#" + context.getChannel().getName() + ").");
             }
             //TODO(AKSKL): resolve other errors
-            return;
         }
     }
 }
