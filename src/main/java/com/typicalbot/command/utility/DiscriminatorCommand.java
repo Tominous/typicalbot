b595232d6cfd24a917a72ad991a86e79fbcde98d
Nicholas Sylke <hello@nsylke.me>
2019-02-25 13:02:56 -0600

    Implement discriminator command

--- src/main/java/com/typicalbot/command/utility/DiscriminatorCommand.java
+++ src/main/java/com/typicalbot/command/utility/DiscriminatorCommand.java
@@ -21,6 +21,11 @@ import com.typicalbot.command.CommandCategory;
 import com.typicalbot.command.CommandConfiguration;
 import com.typicalbot.command.CommandContext;
 import com.typicalbot.command.CommandPermission;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @CommandConfiguration(category = CommandCategory.UTILITY, aliases = "discriminator")
 public class DiscriminatorCommand implements Command {
@@ -31,6 +36,38 @@ public class DiscriminatorCommand implements Command {
 
     @Override
     public void execute(CommandContext context, CommandArgument argument) {
-        throw new UnsupportedOperationException("This command has not been implemented yet.");
+        String discrim;
+
+        if (argument.has()) {
+            String temp = argument.get(0);
+
+            if (temp.length() != 4) {
+                context.sendMessage("Not a valid discriminator.");
+                return;
+            }
+
+            discrim = temp;
+        } else {
+            discrim = context.getMessage().getAuthor().getDiscriminator();
+        }
+
+        List<User> users = new ArrayList<>();
+        context.getGuild().getMembers().stream().filter(m -> m.getUser().getDiscriminator().equals(discrim)).forEach(m -> users.add(m.getUser()));
+
+        if (users.isEmpty()) {
+            context.sendMessage("There are no users with that discriminator.");
+            return;
+        }
+
+        EmbedBuilder builder = new EmbedBuilder();
+
+        builder.setTitle("Users with the discriminator of " + discrim);
+        builder.setColor(CommandContext.TYPICALBOT_SUCCESS);
+
+        users.forEach(u -> {
+            builder.addField(u.getAsTag(), u.getId(), true);
+        });
+
+        context.sendEmbed(builder.build());
     }
 }
