c50cb19233faacc2c8280f83e84d3ecf9a05f813
Nicholas Sylke <hello@nsylke.me>
2019-02-25 11:06:20 -0600

    Implement UrbanDictionary command

--- src/main/java/com/typicalbot/command/integration/UrbandictionaryCommand.java
+++ src/main/java/com/typicalbot/command/integration/UrbandictionaryCommand.java
@@ -21,6 +21,15 @@ import com.typicalbot.command.CommandCategory;
 import com.typicalbot.command.CommandConfiguration;
 import com.typicalbot.command.CommandContext;
 import com.typicalbot.command.CommandPermission;
+import net.dv8tion.jda.core.EmbedBuilder;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.IOException;
 
 @CommandConfiguration(category = CommandCategory.INTEGRATION, aliases = "urbandictionary")
 public class UrbandictionaryCommand implements Command {
@@ -36,6 +45,34 @@ public class UrbandictionaryCommand implements Command {
 
     @Override
     public void execute(CommandContext context, CommandArgument argument) {
-        throw new UnsupportedOperationException("This command has not been implemented yet.");
+        String url;
+
+        if (argument.has()) {
+            url = "http://api.urbandictionary.com/v0/define?term=" + argument.toString();
+        } else {
+            url = "http://api.urbandictionary.com/v0/random";
+        }
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(url).build();
+
+        try {
+            Response response = client.newCall(request).execute();
+
+            JSONArray list = new JSONObject(new JSONTokener(response.body().byteStream())).getJSONArray("list");
+            JSONObject term = list.getJSONObject(0);
+
+            EmbedBuilder builder = new EmbedBuilder();
+
+            builder.setTitle(term.getString("word"), term.getString("permalink"));
+            builder.setDescription(term.getString("definition").replaceAll("\\[", "").replaceAll("]", ""));
+            builder.setColor(CommandContext.TYPICALBOT_SUCCESS);
+            builder.addField("Example", term.getString("example").replaceAll("\\[", "").replaceAll("]", ""), false);
+            builder.addField("Rating", ":arrow_up: `" + term.getInt("thumbs_up") + "` :arrow_down: `" + term.getInt("thumbs_down") + "`", false);
+
+            context.sendEmbed(builder.build());
+        } catch (IOException ex) {
+            context.sendMessage("Failed to retrieve result from Urban Dictionary.");
+        }
     }
 }
