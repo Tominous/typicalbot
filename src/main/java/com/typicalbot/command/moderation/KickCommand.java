e5bdc64837a737b45aecc960b176954db238bd06
Nicholas Sylke <hello@nsylke.me>
2019-03-01 11:21:47 -0600

    Add permission checks to kick command

--- src/main/java/com/typicalbot/command/moderation/KickCommand.java
+++ src/main/java/com/typicalbot/command/moderation/KickCommand.java
@@ -21,6 +21,7 @@ import com.typicalbot.command.CommandCategory;
 import com.typicalbot.command.CommandConfiguration;
 import com.typicalbot.command.CommandContext;
 import com.typicalbot.command.CommandPermission;
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.User;
 
 @CommandConfiguration(category = CommandCategory.MODERATION, aliases = "kick")
@@ -32,19 +33,38 @@ public class KickCommand implements Command {
 
     @Override
     public void execute(CommandContext context, CommandArgument argument) {
+        if (!context.getMember().hasPermission(Permission.KICK_MEMBERS)) {
+            context.sendMessage("You do not have permission to kick members.");
+            return;
+        }
+
+        if (!context.getSelfMember().hasPermission(Permission.KICK_MEMBERS)) {
+            context.sendMessage("TypicalBot does not have permission to kick members.");
+            return;
+        }
+
         if (!argument.has()) {
-            context.sendMessage("Incorrect usage.");
+            context.sendMessage("Incorrect usage. Please check `$help kick` for usage.");
             return;
         }
 
         User temp = context.getUser(argument.get(0));
-
         if (temp == null) {
-            context.sendMessage("Couldn't find that user.");
+            context.sendMessage("The user `{0}` does not exist.", argument.get(0));
+            return;
+        }
+
+        if (!context.getMember().canInteract(context.getGuild().getMember(temp))) {
+            context.sendMessage("You do not have permission to kick that user.");
+            return;
+        }
+
+        if (!context.getSelfMember().canInteract(context.getGuild().getMember(temp))) {
+            context.sendMessage("TypicalBot does not have permission to kick that user.");
             return;
         }
 
         String reason = String.join(" ", argument.getArguments().subList(1, argument.getArguments().size()));
-        context.getGuild().getController().kick(context.getGuild().getMember(temp), reason).queue(o -> context.sendMessage("Successfully kicked {0} for {1}.", temp.getAsTag(), reason));
+        context.getGuild().getController().kick(temp.getId(), reason).queue(o -> context.sendMessage("Successfully kicked {0} for {1}.", temp.getAsTag(), reason));
     }
 }
