7062e86b33afc55ce49ef955ca97e866fa0794d7
Nicholas Sylke <hello@nsylke.me>
2019-03-01 11:59:39 -0600

    Add permission checks to softban

--- src/main/java/com/typicalbot/command/moderation/SoftbanCommand.java
+++ src/main/java/com/typicalbot/command/moderation/SoftbanCommand.java
@@ -21,6 +21,7 @@ import com.typicalbot.command.CommandCategory;
 import com.typicalbot.command.CommandConfiguration;
 import com.typicalbot.command.CommandContext;
 import com.typicalbot.command.CommandPermission;
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.User;
 
 import java.util.concurrent.TimeUnit;
@@ -34,22 +35,41 @@ public class SoftbanCommand implements Command {
 
     @Override
     public void execute(CommandContext context, CommandArgument argument) {
+        if (!context.getMember().hasPermission(Permission.BAN_MEMBERS)) {
+            context.sendMessage("You do not have permission to ban members.");
+            return;
+        }
+
+        if (!context.getSelfMember().hasPermission(Permission.BAN_MEMBERS)) {
+            context.sendMessage("TypicalBot does not have permission to ban members.");
+            return;
+        }
+
         if (!argument.has()) {
-            context.sendMessage("Incorrect usage.");
+            context.sendMessage("Incorrect usage. Please check `$help ban` for usage.");
             return;
         }
 
         User temp = context.getUser(argument.get(0));
-
         if (temp == null) {
-            context.sendMessage("Couldn't find that user.");
+            context.sendMessage("The user `{0}` does not exist.", argument.get(0));
+            return;
+        }
+
+        if (!context.getMember().canInteract(context.getGuild().getMember(temp))) {
+            context.sendMessage("You do not have permission to ban that user.");
+            return;
+        }
+
+        if (!context.getSelfMember().canInteract(context.getGuild().getMember(temp))) {
+            context.sendMessage("TypicalBot does not have permission to ban that user.");
             return;
         }
 
         String reason = String.join(" ", argument.getArguments().subList(1, argument.getArguments().size()));
         context.getGuild().getController().ban(temp.getId(), 1, reason).queue(o -> {
             context.sendMessage("Successfully softbanned {0} for {1}.", temp.getAsTag(), reason);
-            context.getGuild().getController().unban(temp.getId()).reason(reason).queueAfter(4, TimeUnit.SECONDS);
+            context.getGuild().getController().unban(temp.getId()).reason(reason).queueAfter(3, TimeUnit.SECONDS);
         });
     }
 }
