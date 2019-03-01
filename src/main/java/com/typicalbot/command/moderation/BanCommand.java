53c8ac23288fe390290d3409461aded41289cd7f
Nicholas Sylke <hello@nsylke.me>
2019-03-01 10:22:10 -0600

    Add permission checks to ban command

--- src/main/java/com/typicalbot/command/moderation/BanCommand.java
+++ src/main/java/com/typicalbot/command/moderation/BanCommand.java
@@ -21,6 +21,8 @@ import com.typicalbot.command.CommandCategory;
 import com.typicalbot.command.CommandConfiguration;
 import com.typicalbot.command.CommandContext;
 import com.typicalbot.command.CommandPermission;
+import com.typicalbot.util.SentryUtil;
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.User;
 
 @CommandConfiguration(category = CommandCategory.MODERATION, aliases = "ban")
@@ -32,19 +34,69 @@ public class BanCommand implements Command {
 
     @Override
     public void execute(CommandContext context, CommandArgument argument) {
+        if (!context.getMember().hasPermission(Permission.BAN_MEMBERS)) {
+            context.sendMessage("You do not have permission to ban members.");
+            return;
+        }
+
+        if (!context.getSelfMember().hasPermission(Permission.BAN_MEMBERS)) {
+            context.sendMessage("TypicalBot does not have permission to ban members.");
+            return;
+        }
+
         if (!argument.has()) {
-            context.sendMessage("Incorrect usage.");
+            context.sendMessage("Incorrect usage. Please check `$help ban` for usage.");
             return;
         }
 
         User temp = context.getUser(argument.get(0));
-
         if (temp == null) {
-            context.sendMessage("Couldn't find that user.");
+            context.sendMessage("The user `{0}` does not exist.", argument.get(0));
             return;
         }
 
-        String reason = String.join(" ", argument.getArguments().subList(1, argument.getArguments().size()));
-        context.getGuild().getController().ban(temp.getId(), 0, reason).queue(o -> context.sendMessage("Successfully banned {0} for {1}.", temp.getAsTag(), reason));
+        if (!context.getMember().canInteract(context.getGuild().getMember(temp))) {
+            context.sendMessage("You do not have permission to ban that user.");
+            return;
+        }
+
+        if (!context.getSelfMember().canInteract(context.getGuild().getMember(temp))) {
+            context.sendMessage("TypicalBot does not have permission to ban that user.");
+            return;
+        }
+
+        int purge = 1;
+        if (argument.getArguments().size() >= 2 && argument.get(1).matches("^[0-9]")) {
+            try {
+                purge = Integer.parseInt(argument.get(1));
+
+                if (purge < 0) {
+                    context.sendMessage("Cannot purge less than 0 day.");
+                    return;
+                }
+
+                if (purge > 7) {
+                    context.sendMessage("Cannot purge more than 7 days.");
+                    return;
+                }
+            } catch (NumberFormatException ex) {
+                SentryUtil.capture(ex, BanCommand.class);
+            }
+        }
+
+        String reason;
+        if (argument.getArguments().size() >= 2 && !argument.get(1).matches("^[0-9]")) {
+            reason = String.join(" ", argument.getArguments().subList(1, argument.getArguments().size()));
+        } else if (argument.getArguments().size() >= 3) {
+            reason = String.join(" ", argument.getArguments().subList(2, argument.getArguments().size()));
+        } else {
+            reason = "No reason specified.";
+        }
+
+        context.getGuild().getController().ban(temp.getId(), purge, reason).queue(o -> {
+            context.sendMessage("Successfully banned {0} for {1}.", temp.getAsTag(), reason);
+
+            // Mod log
+        });
     }
 }
